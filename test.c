37d3d9bfaa46532978b3a0e67d63f03b33b5d72f
Constantin-Alexandru <constantinalexandruaurel@yahoo.com>
2024-07-04 14:42:34 +0100

    Refactored the test file to match the new logger form.

---
+++ test.c
@@ -0,0 +1,15 @@
+#include "ansi_logger.h"
+
+int main() {
+  const char *pattern =
+      "%l - %n - %m - %02H - %w %02d/%02M/%y %h:%r:%s %u - %f:%b:%i: %c";
+
+  const struct Logger logger = create_logger("Logger", pattern);
+
+  DEBUG(logger, "Debug");
+  LOG(logger, "Log");
+  INFO(logger, "Info");
+  SUCCESS(logger, "Success");
+  WARN(logger, "Warn");
+  ERR(logger, "Error");
+}
